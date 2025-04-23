81a5751f97e4306690869c4173705c4614faa182
Devi <peddireddydevisree8559@gmail.com>
2025-04-23 08:43:30 +0530

    Committed BaseClass

--- src/main/java/genericUtilities/BaseClass.java
+++ src/main/java/genericUtilities/BaseClass.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -44,7 +45,7 @@ public class BaseClass {
 	{
 		String URL = fUtil.readDatafromPropertyFile("url");
 		
-		driver = new EdgeDriver();
+		driver = new FirefoxDriver();
 		
 		sUtil.maximizeWindow(driver);
 		sUtil.addImplicitWait(driver);
